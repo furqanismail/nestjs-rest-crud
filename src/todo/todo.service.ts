1ef3133ee049319062f278197fd57f7a6b80d08c
furqaanismail <furqaanismail.labfik@umi.ac.id>
2024-04-19 22:51:37 +0800

    feat: add create todo service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { TodoEntity } from './todo.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateTodoDto } from './todo.dto';
 
 @Injectable()
 export class TodoService {
@@ -21,5 +22,16 @@ export class TodoService {
         uuid,
       },
     });
+  }
+
+  async create(createTodoDto: CreateTodoDto) {
+    const { title, description, is_done } = createTodoDto;
+    const createTodo = await this.todoModel.create({
+      title,
+      description,
+      is_done,
+    });
+
+    return createTodo;
   }
 }
